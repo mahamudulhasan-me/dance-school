0378c094a0f105b8c256b83c20b7a7e05ef2bc68
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-06-06 21:39:36 +0600

    add header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ["Roboto Condensed", "sans-serif"],
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
