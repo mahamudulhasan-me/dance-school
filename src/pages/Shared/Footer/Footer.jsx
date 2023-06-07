de5b0af02563aee39ce97c67841b1bb42a74394f
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2023-06-07 10:26:12 +0600

    footer complete

--- src/pages/Shared/Footer/Footer.jsx
+++ src/pages/Shared/Footer/Footer.jsx
@@ -1,4 +1,13 @@
 import React from "react";
+import {
+  FaDribbble,
+  FaFacebookF,
+  FaInstagram,
+  FaLinkedinIn,
+  FaTwitter,
+} from "react-icons/fa";
+import { FcFeedback, FcHome, FcMusic, FcPhone } from "react-icons/fc";
+import { Link } from "react-router-dom";
 import logo from "../../../assets/images/icons/light-logo.webp";
 
 const Footer = () => {
@@ -22,48 +31,138 @@ const Footer = () => {
             better choice for your business.
           </p>
           <h3 className="text-lg">STAY CONNECTED</h3>
-          facebook
+          <div className="flex gap-2">
+            <div>
+              <Link className="footer-social ">
+                <FaDribbble size={22} />
+              </Link>
+            </div>
+            <div>
+              <Link className="footer-social ">
+                <FaInstagram size={22} />
+              </Link>
+            </div>
+            <div>
+              <Link className="footer-social ">
+                <FaFacebookF size={22} />
+              </Link>
+            </div>
+            <div>
+              <Link className="footer-social ">
+                <FaLinkedinIn size={22} />
+              </Link>
+            </div>
+            <div>
+              <Link className="footer-social ">
+                <FaTwitter size={22} />
+              </Link>
+            </div>
+          </div>
         </div>
-        <div className="col-span-2 text-sm gap-x-3 gap-y-8 flex ">
+        <div className="col-span-2 text-gray-100  gap-x-3 gap-y-8 flex ">
           <div className="space-y-3 w-full">
             <h3 className="uppercase dark:text-gray-50 font-semibold text-lg pb-5">
-              Contact Us
+              Opening Hours
               <div className="w-10 h-1 bg-rose-500 mt-3"></div>
             </h3>
-            <div className="flex gap-20 items-center">
-              <ul className="space-y-1">
-                <li className="flex gap-3 ">
-                  i<span>72 Pilgrim Avenue, Chevy Chase</span>
+            <div className="flex gap-20 items-start">
+              <ul className="space-y-4">
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Hip Hop Dance : 07:00 – 21:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Ballet : 09:00 – 21:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Break Dance : 18:00 – 24:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Salsa : 18:00 – 22:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Tab Dancing : 19:00 – 23:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Cha Cha : 20:00 – 23:00</span>
                 </li>
               </ul>
-              <ul className="space-y-1">
-                <li className="flex gap-3 ">
-                  i<span>72 Pilgrim Avenue, Chevy Chase</span>
+              <ul className="space-y-4">
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Corridos : 07:00 – 21:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Cumbia : 09:00 – 21:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Duranguense : 18:00 – 24:00</span>
+                </li>
+                <li className="flex gap-2 group">
+                  <FcMusic
+                    size={20}
+                    className="group-hover:rotate-12 transition-all "
+                  />
+                  <span>Quebradita : 18:00 – 22:00</span>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className="space-y-3">
+        <div className="space-y-4 text-gray-50">
           <h3 className="uppercase dark:text-gray-50 font-semibold text-lg mb-5">
             Contact Us
             <div className="w-10 h-1 bg-rose-500 mt-3"></div>
           </h3>
           <p className="flex gap-3 ">
-            i
+            <FcHome size={30} />
             <span>
               72 Pilgrim Avenue, Chevy Chase, <br /> MD 20815
             </span>
           </p>
           <p className="flex gap-3 ">
-            i
+            <FcPhone size={30} />
             <span>
               (226) 906-2721 <br />
               (671) 925-1352
             </span>
           </p>
           <p className="flex gap-3  ">
-            i<span>info@dance.com</span>
+            <FcFeedback size={30} /> <span>info@dance.com</span>
           </p>
         </div>
       </div>
