cc47b138ef5ad511f8e7b50364c35fb7b2e04e2f
Mahamdul Hasan <mahamudulhasan.org@gmail.com>
2023-06-10 22:47:23 +0600

    todays last commit

--- src/pages/Home/PopularInstructors/PopularInstructors.jsx
+++ src/pages/Home/PopularInstructors/PopularInstructors.jsx
@@ -34,7 +34,6 @@ const PopularInstructors = () => {
                     <FaFacebook />
                   </Link>
                   <Link>
-                    {" "}
                     <FaInstagram />
                   </Link>
                   <Link>
